e7be1d59dfdac13bc8ec2457776138599c8875da
gleidson fagno <gleidsonfagner838@gmail.com>
2022-11-20 20:28:19 -0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style.scss'
+
 function titleComponente(){
     const elemH1 = document.createElement('h1')
     elemH1.innerHTML = 'Helo, word'
+    elemH1.classList.add('title')
     return elemH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
+
 
 module.exports = {
     entry: './src/index.js',
@@ -7,10 +9,32 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|c|sc)ss$/,  //expressao reguar - regex
+                use:[
+                    MiniCssExtract.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.ss$/i,  
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    },
     plugin: [
         new HtmlWebpack({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: 'style.css'
         })
     ]
 }
